6ffbe9ae89a7f455d3a136abd4c4981a79e0cd03
Brian <befrazho@gmail.com>
2023-12-31 00:14:37 -0500

    try to include shared libraries in final build for aarch64 mac

--- solipath.bat
+++
@@ -1,2 +0,0 @@
-if not exist %HOMEDRIVE%%HOMEPATH%\solipath\solipath_setup.bat curl --create-dirs --url "https://raw.githubusercontent.com/Solipath/Solipath-Scripts/main/solipath_setup.bat" -o %HOMEDRIVE%%HOMEPATH%/solipath/solipath_setup.bat	
-%HOMEDRIVE%%HOMEPATH%\solipath\solipath_setup.bat %*
